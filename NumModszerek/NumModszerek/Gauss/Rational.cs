7e31d4ae8b55a2c1f389f1f46c8f65ae879c5f94
Zoltán Nemes <zoltan.nemes@stud.uni-corvinus.hu>
2021-10-01 13:10:30 +0200

    Rational osztály létrehozva

---
+++ NumModszerek/NumModszerek/Gauss/Rational.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rational
+{
+    public class Rational
+    {
+        public int denominator { get; set; }
+        public int nominator { get; set; }
+
+        public Rational(int a, int b)
+        {
+            int m = lnko(a, b);
+            this.denominator = b / m;
+            this.nominator = a / m;
+        }
+
+        public Rational(int a)
+        {
+            this.denominator = 1;
+            this.nominator = a;
+        }
+
+        public static Rational operator +(Rational a, Rational b)
+        {
+            int nevezo = lkkt(a.denominator, b.denominator);
+            int aszorzo = nevezo / a.denominator;
+            int bszorzo = nevezo / b.denominator;
+            return new Rational(a.nominator * aszorzo + b.nominator * bszorzo, nevezo);
+        }
+
+        public static Rational operator -(Rational a, Rational b)
+        {
+            int nevezo = lkkt(a.denominator, b.denominator);
+            int aszorzo = nevezo / a.denominator;
+            int bszorzo = nevezo / b.denominator;
+            return new Rational(a.nominator * aszorzo - b.nominator * bszorzo, nevezo);
+        }
+
+        public static Rational operator *(Rational a, Rational b)
+        {
+            return new Rational(a.nominator * b.nominator, a.denominator * b.denominator);
+        }
+        public static Rational operator /(Rational a, Rational b)
+        {
+            return new Rational(a.nominator * b.denominator, a.denominator * b.nominator);
+        }
+
+        public override string ToString()
+        {
+            if (denominator == 1)
+            {
+                return nominator.ToString();
+            }
+            return $"{nominator}/{denominator}";
+        }
+
+        public static int lkkt(int a, int b)
+        {
+            return a * b / lnko(a, b);
+        }
+
+        public static int lnko(int a, int b)
+        {
+            if (a < b)
+            {
+                int c = b;
+                b = a;
+                a = c;
+            }
+            int m;
+            while (b != 0)
+            {
+                m = a % b;
+                a = b;
+                b = m;
+            }
+            return a;
+        }
+    }
+}
