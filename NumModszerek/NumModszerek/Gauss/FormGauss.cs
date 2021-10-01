46aaccb30f77e83df6bdd135b886c09af118bcad
Zoltán Nemes <zoltan.nemes@stud.uni-corvinus.hu>
2021-10-01 15:38:00 +0200

    Nézet fix

--- NumModszerek/NumModszerek/Gauss/FormGauss.cs
+++ NumModszerek/NumModszerek/Gauss/FormGauss.cs
@@ -31,18 +31,31 @@ namespace NumModszerek.Gauss
                 n = 3;
             }
             GaussModell model = new GaussModell(n);
+            foreach (var item in model.resultVector)
+            {
+                Console.WriteLine(item.ToString());
+            };
             for (int i = 0; i < n; i++)
             {
-                for (int j = 0; j < n; j++)
+                for (int j = 0; j < n+1; j++)
                 {
                     Button b = new Button();
-                    b.Width = panel1.Width / n;
+                    b.Width = panel1.Width / (n+1);
                     b.Height = b.Width;
-                    b.Left = i * b.Width;
-                    b.Top = j * b.Height;
-                    b.Text = model.matrix[i,j].ToString();
+                    b.Left = j * b.Width;
+                    b.Top = i * b.Height;
+                    if(j < n)
+                    {
+                        b.Text = model.matrix[i, j].ToString();
+                    }
+                    else
+                    {
+                        b.Text = model.bVector[i].ToString();
+                    }
+                    
                     panel1.Controls.Add(b);
                 }
+                
             }
         }
     }
